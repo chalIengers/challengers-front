acd8a3665373056dc9903fc13673d44e0cd7fc16
hin6150 <hin6150@gmail.com>
2023-07-04 01:53:37 +0900

    Feat - 프로젝트 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./component";
+import Project from "./component/project";
+import Publish from "./component/project/publish/Publish";
+import Detail from "./component/project/detail/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/project/*" element={<Project />}>
+            <Route path="detail" element={<Detail />} />
+            <Route path="publish" element={<Publish />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
