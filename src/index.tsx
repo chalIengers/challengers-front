a7d6cb268b109ddf225e7eee04725645570970fc
hin6150 <hin6150@gmail.com>
2023-07-04 09:37:34 +0900

    Design - Reset CSS, Theme 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,7 +12,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "@emotion/react";
+
+const theme: DefaultTheme = {
+  fontSizes: {
+    xxs: "12px",
+    xs: "14px",
+    sm: "15px",
+    base: "16px",
+    md: "20px",
+    lg: "25px",
+  },
+  colors: {
+    black: "#000",
+    dark: "#212121",
+    gray: "rgba(217, 217, 217, 0.40)",
+    primary: "#4F85E8",
+    secondary: "#E8F3FF",
+  },
+};
+
+export default theme;
