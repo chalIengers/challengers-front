bc1ad13096aa5743bd8ab28cfe6220aaaa82de3c
hin6150 <hin6150@gmail.com>
2023-07-04 13:14:40 +0900

    Feat - ButtonComponent 생성(#1)

--- src/component/emotion/component.tsx
+++ src/component/emotion/component.tsx
@@ -2,10 +2,11 @@
 import { css } from "@emotion/react";
 import React, { ReactNode } from "react";
 
-type ComponentProps = {
+type InnerProps = {
   children: ReactNode;
 };
-export const Inner = ({ children }: ComponentProps) => (
+
+export const Inner = ({ children }: InnerProps) => (
   <div
     css={css`
       width: 1200px;
@@ -13,8 +14,52 @@ export const Inner = ({ children }: ComponentProps) => (
       margin-top: 50px;
       display: flex;
       flex-direction: column;
+      align-items: center;
     `}
   >
     {children}
   </div>
 );
+
+type ButtonBoxProps = {
+  text: string;
+  type: "large" | "small";
+};
+
+export const ButtonBox = ({ text, type }: ButtonBoxProps) => {
+  const styles = {
+    large: css`
+      width: 1198px;
+      height: 99px;
+      border-radius: 12px;
+      font-size: 25px;
+      letter-spacing: -0.75px;
+    `,
+    small: css`
+      width: 198px;
+      height: 53px;
+      border-radius: 7px;
+      font-size: 17px;
+      letter-spacing: -0.51px;
+    `,
+  };
+
+  return (
+    <button
+      type="button"
+      css={css`
+        ${styles[type]}
+        background: #4a7edc;
+        color: #fff;
+        font-weight: 700;
+
+        &:active {
+          transform: scale(0.98);
+          box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+        }
+      `}
+    >
+      {text}
+    </button>
+  );
+};
