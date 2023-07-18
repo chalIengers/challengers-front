31734ee19e8536e22f6ca9ee7c697ddbcf71d4b4
hin6150 <hin6150@gmail.com>
2023-07-19 08:00:35 +0900

    Style - 디자인 시스템 수정(#15)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,58 +1,61 @@
 import { DefaultTheme } from '@emotion/react';
 
+const font = {
+  xxs: '1.1rem',
+  xs: '1.2rem',
+  sm: '1.4rem',
+  md: '1.6rem',
+  lg: '1.8rem',
+  xl: '2.0rem',
+  xxl: '2.2rem',
+  xxxl: '4.0rem',
+};
+
 const theme: DefaultTheme = {
-  fontSizes: {
-    xxs: '1.1rem',
-    xs: '1.2rem',
-    sm: '1.4rem',
-    md: '1.6rem',
-    lg: '1.8rem',
-    xl: '2.0rem',
-    xxl: '2.2rem',
-    xxxl: '2.4rem',
-  },
   textVariants: {
-    heading1: { fontSizes: '2.4rem', fontWeight: '700' },
-    heading2: { fontSizes: '2.2rem', fontWeight: '700' },
-    heading3: { fontSizes: '2.0rem', fontWeight: '700' },
-    heading4: { fontSizes: '1.8rem', fontWeight: '400' },
-    body1: { fontSizes: '1.6rem', fontWeight: '700' },
-    body2: { fontSizes: '1.6rem', fontWeight: '400' },
-    body3: { fontSizes: '1.4rem', fontWeight: '700' },
-    body4: { fontSizes: '1.4rem', fontWeight: '400' },
-    detail1: { fontSizes: '1.2rem', fontWeight: '700' },
-    detail2: { fontSizes: '1.2rem', fontWeight: '400' },
+    heading1: { 'font-size': font.xxxl, 'font-weight': '700', 'letter-spacing': '-0.12rem' },
+    heading2: { 'font-size': font.xxl, 'font-weight': '700', 'letter-spacing': '-0.066rem' },
+    heading3: { 'font-size': font.xl, 'font-weight': '700', 'letter-spacing': '-0.06rem' },
+    heading4: { 'font-size': font.lg, 'font-weight': '400', 'letter-spacing': '-0.054rem' },
+    body1: { 'font-size': font.md, 'font-weight': '700', 'letter-spacing': '-0.048rem' },
+    body1_bold: { 'font-size': font.md, 'font-weight': '400', 'letter-spacing': '-0.048rem' },
+    body2: { 'font-size': font.sm, 'font-weight': '700', 'letter-spacing': '-0.042rem' },
+    body2_bold: { 'font-size': font.sm, 'font-weight': '400', 'letter-spacing': '-0.042rem' },
+    detail: { 'font-size': font.xs, 'font-weight': '700', 'letter-spacing': '-0.036rem' },
+    detail_bold: { 'font-size': font.xs, 'font-weight': '400', 'letter-spacing': '-0.036rem' },
   },
-  colorMain: {
-    primary_100: '#DBECFD',
-    primary_200: '#B8D7FB',
-    primary_300: '#93BCF4',
-    primary_400: '#75A3EA',
-    primary_500: '#4A7EDC',
-    primary_600: '#3661BD',
-    primary_700: '#25479E',
-    primary_800: '#17317F',
-    primary_900: '#0E2169',
-  },
-  colorSemantic: {
-    success_500: '#59BA2C',
-    info_500: '#47C1FF',
-    warning_500: '#FFB200',
-    danger_500: '#FF3A3A',
-  },
-  colorGray: {
-    gray_900: '#222222',
-    gray_800: '#424242',
-    gray_700: '#616161',
-    gray_600: '#757575',
-    gray_500: '#9e9e9e',
-    gray_400: '#bdbdbd',
-    gray_300: '#e0e0e0',
-    gray_200: '#eeeeee',
-    gray_100: '#f5f5f5',
-    gray_50: '#fafafa',
-    black: '#000000',
-    white: '#ffffff',
+  palette: {
+    primary: {
+      100: '#DBECFD',
+      200: '#B8D7FB',
+      300: '#93BCF4',
+      400: '#75A3EA',
+      500: '#4A7EDC',
+      600: '#3661BD',
+      700: '#25479E',
+      800: '#17317F',
+      900: '#0E2169',
+    },
+    gray: {
+      900: '#222222',
+      800: '#424242',
+      700: '#616161',
+      600: '#757575',
+      500: '#9e9e9e',
+      400: '#bdbdbd',
+      300: '#e0e0e0',
+      200: '#eeeeee',
+      100: '#f5f5f5',
+      50: '#fafafa',
+      black: '#000000',
+      white: '#ffffff',
+    },
+    semantic: {
+      success: { 500: '#59BA2C' },
+      info: { 500: '#47C1FF' },
+      warning: { 500: '#FFB200' },
+      danger: { 500: '#FF3A3A' },
+    },
   },
   spacing: {
     xxxs: '0.2rem',
@@ -68,3 +71,20 @@ const theme: DefaultTheme = {
 };
 
 export default theme;
+
+// 예시
+// primary: {
+//   "font-weight": '700',
+//   "letter-spacing"white',
+//   backgroundColor: 'primary.500',
+//   '&:hover': {
+//     backgroundColor: 'primary.700',
+//   },
+//   '&:active': {
+//     backgroundColor: 'primary.800',
+//   },
+//   '&:disabled': {
+//     color: 'primary.200',
+//     backgroundColor: 'primary.100',
+//   },
+// },
--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,64 +1,56 @@
 import '@emotion/react';
 
 type Text = {
-  fontSizes: string;
-  fontWeight: string;
+  'font-size': string;
+  'font-weight': string;
+  'letter-spacing': string;
 };
 declare module '@emotion/react' {
   export interface DefaultTheme {
-    fontSizes: {
-      xxs: string;
-      xs: string;
-      sm: string;
-      md: string;
-      lg: string;
-      xl: string;
-      xxl: string;
-      xxxl: string;
-    };
     textVariants: {
       heading1: Text;
       heading2: Text;
       heading3: Text;
       heading4: Text;
       body1: Text;
+      body1_bold: Text;
       body2: Text;
-      body3: Text;
-      body4: Text;
-      detail1: Text;
-      detail2: Text;
+      body2_bold: Text;
+      detail: Text;
+      detail_bold: Text;
     };
-
-    colorMain: {
-      primary_100: string;
-      primary_200: string;
-      primary_300: string;
-      primary_400: string;
-      primary_500: string;
-      primary_600: string;
-      primary_700: string;
-      primary_800: string;
-      primary_900: string;
-    };
-    colorSemantic: {
-      success_500: string;
-      info_500: string;
-      warning_500: string;
-      danger_500: string;
-    };
-    colorGray: {
-      gray_900: string;
-      gray_800: string;
-      gray_700: string;
-      gray_600: string;
-      gray_500: string;
-      gray_400: string;
-      gray_300: string;
-      gray_200: string;
-      gray_100: string;
-      gray_50: string;
-      black: string;
-      white: string;
+    palette: {
+      primary: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
+      gray: {
+        900: string;
+        800: string;
+        700: string;
+        600: string;
+        500: string;
+        400: string;
+        300: string;
+        200: string;
+        100: string;
+        50: string;
+        black: string;
+        white: string;
+      };
+      semantic: {
+        success: { 500: string };
+        info: { 500: string };
+        warning: { 500: string };
+        danger: { 500: string };
+      };
     };
     spacing: {
       xxxs: string;
