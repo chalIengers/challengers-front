29f52be5d72b58da48c0b436810d52e216a983e6
hin6150 <hin6150@gmail.com>
2023-07-21 09:12:26 +0900

    Chore - Theme에서 여백 제거(#15)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,28 +1,29 @@
 import { DefaultTheme } from '@emotion/react';
 
-const font = {
-  xxs: '1.1rem',
-  xs: '1.2rem',
-  sm: '1.4rem',
-  md: '1.6rem',
-  lg: '1.8rem',
-  xl: '2.0rem',
-  xxl: '2.2rem',
-  xxxl: '4.0rem',
-};
-
-const theme: DefaultTheme = {
+export const theme: DefaultTheme = {
+  font: {
+    xxs: '1.1rem',
+    xs: '1.2rem',
+    sm: '1.4rem',
+    md: '1.6rem',
+    lg: '1.8rem',
+    xl: '2.0rem',
+    xxl: '2.2rem',
+    xxxl: '2.2rem',
+    xxxxl: '4.0rem',
+  },
   textVariants: {
-    heading1: { fontSize: font.xxxl, fontWeight: '700', letterSpacing: '-0.12rem' },
-    heading2: { fontSize: font.xxl, fontWeight: '700', letterSpacing: '-0.066rem' },
-    heading3: { fontSize: font.xl, fontWeight: '700', letterSpacing: '-0.06rem' },
-    heading4: { fontSize: font.lg, fontWeight: '400', letterSpacing: '-0.054rem' },
-    body1: { fontSize: font.md, fontWeight: '700', letterSpacing: '-0.048rem' },
-    body1_bold: { fontSize: font.md, fontWeight: '400', letterSpacing: '-0.048rem' },
-    body2: { fontSize: font.sm, fontWeight: '700', letterSpacing: '-0.042rem' },
-    body2_bold: { fontSize: font.sm, fontWeight: '400', letterSpacing: '-0.042rem' },
-    detail: { fontSize: font.xs, fontWeight: '700', letterSpacing: '-0.036rem' },
-    detail_bold: { fontSize: font.xs, fontWeight: '400', letterSpacing: '-0.036rem' },
+    heading1: { fontSize: '4rem', fontWeight: '700', letterSpacing: '-0.12rem' },
+    heading2: { fontSize: '2.4rem', fontWeight: '700', letterSpacing: '-0.12rem' },
+    heading3: { fontSize: '2.2rem', fontWeight: '700', letterSpacing: '-0.066rem' },
+    heading4: { fontSize: '2.0rem', fontWeight: '700', letterSpacing: '-0.06rem' },
+    heading5: { fontSize: '1.8rem', fontWeight: '400', letterSpacing: '-0.054rem' },
+    body1: { fontSize: '1.6rem', fontWeight: '700', letterSpacing: '-0.048rem' },
+    body1_bold: { fontSize: '1.6rem', fontWeight: '400', letterSpacing: '-0.048rem' },
+    body2: { fontSize: '1.4rem', fontWeight: '700', letterSpacing: '-0.042rem' },
+    body2_bold: { fontSize: '1.4rem', fontWeight: '400', letterSpacing: '-0.042rem' },
+    detail: { fontSize: '1.2rem', fontWeight: '700', letterSpacing: '-0.036rem' },
+    detail_bold: { fontSize: '1.2rme', fontWeight: '400', letterSpacing: '-0.036rem' },
   },
   palette: {
     primary: {
@@ -37,8 +38,8 @@ const theme: DefaultTheme = {
       900: '#0E2169',
     },
     gray: {
-      900: '#222222',
-      800: '#424242',
+      900: '#212121',
+      800: '#404040',
       700: '#616161',
       600: '#757575',
       500: '#9e9e9e',
@@ -57,17 +58,17 @@ const theme: DefaultTheme = {
       danger: { 500: '#FF3A3A' },
     },
   },
-  spacing: {
-    xxxs: '0.2rem',
-    xxs: '0.4rem',
-    xs: '0.8rem',
-    sm: '1.2rem',
-    md: '1.6rem',
-    lg: '2.0rem',
-    xl: '2.4rem',
-    xxl: '3.2rem',
-    xxxl: '4.0rem',
-  },
+  // spacing: {
+  //   xxxs: '0.2rem',
+  //   xxs: '0.4rem',
+  //   xs: '0.8rem',
+  //   sm: '1.2rem',
+  //   md: '1.6rem',
+  //   lg: '2.0rem',
+  //   xl: '2.4rem',
+  //   xxl: '3.2rem',
+  //   xxxl: '4.0rem',
+  // },
 };
 
 export default theme;
--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -7,17 +7,29 @@ type Text = {
 };
 declare module '@emotion/react' {
   export interface DefaultTheme {
+    font: {
+      xxs: '1.1rem';
+      xs: '1.2rem';
+      sm: '1.4rem';
+      md: '1.6rem';
+      lg: '1.8rem';
+      xl: '2.0rem';
+      xxl: '2.2rem';
+      xxxl: '2.2rem';
+      xxxxl: '4.0rem';
+    };
     textVariants: {
-      heading1: Text;
-      heading2: Text;
-      heading3: Text;
-      heading4: Text;
-      body1: Text;
-      body1_bold: Text;
-      body2: Text;
-      body2_bold: Text;
-      detail: Text;
-      detail_bold: Text;
+      heading1: { fontSize: '4rem'; fontWeight: '700'; letterSpacing: '-0.12rem' };
+      heading2: { fontSize: '2.4rem'; fontWeight: '700'; letterSpacing: '-0.12rem' };
+      heading3: { fontSize: '2.2rem'; fontWeight: '700'; letterSpacing: '-0.066rem' };
+      heading4: { fontSize: '2.0rem'; fontWeight: '700'; letterSpacing: '-0.06rem' };
+      heading5: { fontSize: '1.8rem'; fontWeight: '400'; letterSpacing: '-0.054rem' };
+      body1: { fontSize: '1.6rem'; fontWeight: '700'; letterSpacing: '-0.048rem' };
+      body1_bold: { fontSize: '1.6rem'; fontWeight: '400'; letterSpacing: '-0.048rem' };
+      body2: { fontSize: '1.4rem'; fontWeight: '700'; letterSpacing: '-0.042rem' };
+      body2_bold: { fontSize: '1.4rem'; fontWeight: '400'; letterSpacing: '-0.042rem' };
+      detail: { fontSize: '1.2rem'; fontWeight: '700'; letterSpacing: '-0.036rem' };
+      detail_bold: { fontSize: '1.2rme'; fontWeight: '400'; letterSpacing: '-0.036rem' };
     };
     palette: {
       primary: {
@@ -32,18 +44,18 @@ declare module '@emotion/react' {
         900: string;
       };
       gray: {
-        900: string;
-        800: string;
-        700: string;
-        600: string;
-        500: string;
-        400: string;
-        300: string;
-        200: string;
-        100: string;
-        50: string;
-        black: string;
-        white: string;
+        900: '#212121';
+        800: '#404040';
+        700: '#616161';
+        600: '#757575';
+        500: '#9e9e9e';
+        400: '#bdbdbd';
+        300: '#e0e0e0';
+        200: '#eeeeee';
+        100: '#f5f5f5';
+        50: '#fafafa';
+        black: '#000000';
+        white: '#ffffff';
       };
       semantic: {
         success: { 500: string };
@@ -52,16 +64,16 @@ declare module '@emotion/react' {
         danger: { 500: string };
       };
     };
-    spacing: {
-      xxxs: string;
-      xxs: string;
-      xs: string;
-      sm: string;
-      md: string;
-      lg: string;
-      xl: string;
-      xxl: string;
-      xxxl: string;
-    };
+    // spacing: {
+    //   xxxs: '0.2rem';
+    //   xxs: '0.4rem';
+    //   xs: '0.8rem';
+    //   sm: '1.2rem';
+    //   md: '1.6rem';
+    //   lg: '2.0rem';
+    //   xl: '2.4rem';
+    //   xxl: '3.2rem';
+    //   xxxl: '4.0rem';
+    // };
   }
 }
