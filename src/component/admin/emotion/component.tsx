adc1f504dffa65fbfb26aabc4ad7f669686354b4
joeunSong <thdwhdms99@naver.com>
2023-07-26 23:25:13 +0900

    Rename - Publish.tsx 파일이름을 ProjectPublish.tsx로 변경 & club페이지와 관리자 페이지 디렉토리 생성

---
+++ src/component/admin/emotion/component.tsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+
+import React from 'react';
+import { css } from '@emotion/react';
+
+export const ToastContainer = () => {
+  return (
+    <div
+      css={css`
+        border: 1px solid black;
+      `}
+    >
+      Test
+    </div>
+  );
+};
