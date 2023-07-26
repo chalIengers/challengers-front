821ede45cc9e5341559387f606dccac85a54ce6c
joeunSong <thdwhdms99@naver.com>
2023-07-26 23:25:13 +0900

    Rename - Publish.tsx 파일이름을 ProjectPublish.tsx로 변경 & club페이지와 관리자 페이지 디렉토리 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './component';
 import Project from './component/project';
-import Publish from './component/project/publish/Publish';
+import ProjectPublish from './component/project/publish/ProjectPublish';
 import Detail from './component/project/detail/Detail';
+import Club from './component/club';
 import { Header } from './component/emotion/component';
+import SignUp from './component/club/signUp/SignUp';
+import ClubPublish from './component/club/publish/ClubPublish';
+import Admin from './component/admin';
+import AdminClub from './component/admin/club/AdminClub';
 
 function App() {
   return (
@@ -16,7 +21,12 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/project" element={<Project />} />
           <Route path="/project/detail" element={<Detail />} />
-          <Route path="/project/publish" element={<Publish />} />
+          <Route path="/project/publish" element={<ProjectPublish />} />
+          <Route path="/club" element={<Club />} />
+          <Route path="/club/signUp" element={<SignUp />} />
+          <Route path="/club/publish" element={<ClubPublish />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/club" element={<AdminClub />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/component/admin/club/AdminClub.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminClub = () => {
+  return <div>AdminClub</div>;
+};
+
+export default AdminClub;
