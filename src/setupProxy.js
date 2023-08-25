65e7ff2e123e44f282a8fb0e7261f24371c530ac
hin6150 <hin6150@gmail.com>
2023-08-26 01:43:56 +0900

    Chore - 파일 이름 수정(#28)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api/v1', // 프록시를 설정할 엔드포인트 경로
+    '/api', // 프록시를 설정할 엔드포인트 경로
     createProxyMiddleware({
       target: 'http://101.101.216.151:8080', // 실제 API 서버의 주소
       changeOrigin: true, // 도메인 변경
